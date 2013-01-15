b9a2b57d0b1394ce76299dceb5ed8b87e9768003
Helmut Manck <helmut.manck@eonas.de>
2013-01-15 21:06:34 +0100

    - prepatched tomcat added

---
+++ pluto-tomcat6/src/main/assembly/setenv.sh
@@ -0,0 +1,1 @@
+CATALINA_OPTS="-Djava.awt.headless=true -XX:MaxPermSize=512M -Xmx2G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Dorg.apache.jasper.runtime.BodyContentImpl.LIMIT_BUFFER=true"
