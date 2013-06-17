94499d9cf56402c259c7ab1b8e0f07fbfcb44cb2
Helmut Manck <helmut.manck@eonas.de>
2013-06-17 19:16:36 +0200

    pluto-tomcat6 forked to own repo

--- pluto-tomcat6/src/main/assembly/setenv.sh
+++
@@ -1,1 +0,0 @@
-CATALINA_OPTS="-Djava.awt.headless=true -XX:MaxPermSize=512M -Xmx2G -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Dorg.apache.jasper.runtime.BodyContentImpl.LIMIT_BUFFER=true"
