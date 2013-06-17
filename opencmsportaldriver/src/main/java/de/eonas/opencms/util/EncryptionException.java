f8f625eaa6370830e0fae92420f8a0807e56a7ff
Helmut Manck <helmut.manck@eonas.de>
2013-06-17 18:40:57 +0200

    pushed to own project

--- opencmsportaldriver/src/main/java/de/eonas/opencms/util/EncryptionException.java
+++
@@ -1,9 +0,0 @@
-package de.eonas.opencms.util;
-
-public class EncryptionException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public EncryptionException(Throwable clause) {
-		super(clause);
-	}
-}
